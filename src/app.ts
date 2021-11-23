c6477f89f1a17397b667323ef278981ae275711b
Nikolajs Korolkovs <neverate@gmail.com>
2021-11-23 22:38:13 +0200

    linting style changes

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,38 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import express, { Express } from 'express';
 import { Server } from 'http';
 import { ExceptionFilter } from './errors/exception.filter.js';
 import { ILogger } from './logger/logger.interface.js';
 import { injectable, inject } from 'inversify';
-import { TYPES } from "./types";
-import { IUserController } from "./users/users.controller.interface";
+import { TYPES } from './types';
+import { IUserController } from './users/users.controller.interface';
 @injectable()
 export class App {
-    app: Express;
-    server: Server;
-    port: number;
+	app: Express;
+	server: Server;
+	port: number;
 
-    constructor(@inject(TYPES.ILogger) private logger: ILogger,
-        @inject(TYPES.IUserController) private userController: IUserController,
-        @inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter ) {
-        this.app = express();
-        this.port = 8000;
-    }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.IUserController) private userController: IUserController,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-    useRoutes() {
-        this.app.use('/users', this.userController.router);
-    }
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
 
-    useExceptionFilters() {
-        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
-    }
+	useExceptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
 
-    public async init() {
-        this.useRoutes();
-        this.useExceptionFilters();
-        this.server = this.app.listen(this.port);
-        this.logger.log(`Server is running at https://localhost:${this.port}`);
-    }
+	public async init(): Promise<void> {
+		this.useRoutes();
+		this.useExceptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Server is running at https://localhost:${this.port}`);
+	}
 }
--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,39 +1,39 @@
 import { Router, Response } from 'express';
-import { IControllerRoute } from "./route.interface";
-import { ILogger } from "../logger/logger.interface";
+import { ExpressReturnType, IControllerRoute } from './route.interface';
+import { ILogger } from '../logger/logger.interface';
 import { injectable } from 'inversify';
 
 @injectable()
 export abstract class BaseController {
-    private readonly _router: Router;
+	private readonly _router: Router;
 
-    constructor(private logger: ILogger) {
-        this._router = Router();        
-    }
+	constructor(private logger: ILogger) {
+		this._router = Router();
+	}
 
-    get router() {
-        return this._router;
-    }
-    
-    public send<T>(res: Response, code: number, message: T) {
-        res.type('application/json');
-        return res.status(code).json(message);
-    }
+	get router(): Router {
+		return this._router;
+	}
 
-    public ok<T>(res: Response, message: T) {
-        this.send<T>(res, 200, message);
-    }
+	public send<T>(res: Response, code: number, message: T): ExpressReturnType {
+		res.type('application/json');
+		return res.status(code).json(message);
+	}
 
-    public created(res: Response) {
-        return res.sendStatus(201);
-    }
+	public ok<T>(res: Response, message: T): ExpressReturnType {
+		return this.send<T>(res, 200, message);
+	}
 
-    protected bindRoutes(routes: IControllerRoute[]) {
-        for(const route of routes) {
-            this.logger.log(`[${route.method}] ${route.path}`);
-            // to prevent losing the this context of controller
-            const handler = route.func.bind(this);
-            this.router[route.method](route.path, handler);
-        }
-    }
+	public created(res: Response): ExpressReturnType {
+		return res.sendStatus(201);
+	}
+
+	protected bindRoutes(routes: IControllerRoute[]): void {
+		for (const route of routes) {
+			this.logger.log(`[${route.method}] ${route.path}`);
+			// to prevent losing the this context of controller
+			const handler = route.func.bind(this);
+			this.router[route.method](route.path, handler);
+		}
+	}
 }
