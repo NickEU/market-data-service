417db25997fc23a4c1a84301cd0a20693e8f3537
Nikolajs Korolkovs <neverate@gmail.com>
2021-11-24 16:06:45 +0200

    added user entity and test user creation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import { ILogger } from '../logger/logger.interface';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
 // import fs from 'fs';
 // import { resolve } from 'path';
 
@@ -22,14 +23,20 @@ export class UserController extends BaseController implements IUserController {
 		]);
 	}
 
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, _next: NextFunction): void {
-		console.log(req.body);
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		_next: NextFunction,
+	): Promise<void> {
+		console.log(body);
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
 		// data.push(fs.readFileSync(resolve(__dirname, '../../unlock.zip')));
-		this.ok(res, 'Registration successful');
+		this.ok(res, newUser);
 	}
 
-	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
+	login({ body }: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(body);
 		next(new HTTPError(401, 'Authorization error!', 'login'));
 	}
 }
