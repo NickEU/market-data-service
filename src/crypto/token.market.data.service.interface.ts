7edd3b8c2951fbe843c575115db546054b03f686
Nikolajs Korolkovs <neverate@gmail.com>
2022-03-26 15:21:41 +0200

    implemented basic version of token candle record creation in DB

--- src/crypto/token.market.data.service.interface.ts
+++ src/crypto/token.market.data.service.interface.ts
@@ -1,8 +1,18 @@
+import { TokenCandleModel } from '@prisma/client';
+
 export interface ITokenMarketDataService {
 	ping: () => Promise<object>;
 	getLiveMarketDataForToken: (
 		tokenName: string,
 		timePeriod: string,
-	) => Promise<Array<Array<Number>>>;
-	createCandleRecordInDb: (candleData: Array<Number>) => Promise<boolean>;
+	) => Promise<Array<Array<number>>>;
+	createCandleRecordInDb: (
+		tokenCode: string,
+		candleData: Array<number>,
+		statType: number,
+	) => Promise<TokenCandleModel | null>;
+	findLastCandleRecordInDb: (
+		tokenCode: string,
+		statType: number,
+	) => Promise<TokenCandleModel | null>;
 }
