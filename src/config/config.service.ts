9eaf784d59dd9f5203122a4b499ffc89eed40c0d
Nikolajs Korolkovs <neverate@gmail.com>
2022-03-29 18:10:32 +0300

    Config service no longer uses ILogger to allow for env-specific logging

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,18 +1,16 @@
 import { IConfigService } from './config.service.interface';
 import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
-import { inject, injectable } from 'inversify';
-import { TYPES } from '../types';
-import { ILogger } from '../logger/logger.interface';
+import { injectable } from 'inversify';
 
 @injectable()
 export class ConfigService implements IConfigService {
 	private config: DotenvParseOutput;
-	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+	constructor() {
 		const result: DotenvConfigOutput = config();
 		if (result.error) {
-			this.logger.error('[ConfigService] Unable to read .env file.');
+			console.log('[ConfigService] Unable to read .env file.');
 		} else {
-			this.logger.log('[ConfigService] Loaded configuration from .env file.');
+			console.log('[ConfigService] Loaded configuration from .env file.');
 			this.config = result.parsed as DotenvParseOutput;
 		}
 	}
